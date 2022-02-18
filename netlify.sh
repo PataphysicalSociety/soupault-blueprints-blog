95878d96e55906ff6b2d845517af884daffdd308
Daniil Baturin <daniil@baturin.org>
2022-02-18 11:01:17 +0700

    Update soupault version in netlify.sh 4.0.0 is required for the new features of this blueprint

--- netlify.sh
+++ netlify.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SOUPAULT_VERSION="2.0.0"
+SOUPAULT_VERSION="4.0.0-beta1"
 
 if [ -z "${SOUPAULT_VERSION}" ]; then
     echo "Error: soupault version is undefined, cannot decide what to download"
@@ -8,7 +8,7 @@ if [ -z "${SOUPAULT_VERSION}" ]; then
 fi
 
 echo "Downloading and unpacking soupault"
-wget https://github.com/dmbaturin/soupault/releases/download/$SOUPAULT_VERSION/soupault-$SOUPAULT_VERSION-linux-x86_64.tar.gz
+wget https://github.com/PataphysicalSociety/soupault/releases/download/$SOUPAULT_VERSION/soupault-$SOUPAULT_VERSION-linux-x86_64.tar.gz
 if [ $? != 0 ]; then
     echo "Error: failed to download soupault."
     exit 1
